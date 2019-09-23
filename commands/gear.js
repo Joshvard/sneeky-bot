5b09745833deaf58607de57c5534e31e40ea4bee
zjarv <irishstiltskin@gmail.com>
2019-09-23 19:04:46 +0100

    wip: creating a gear upload/download command to store gear screenshots

---
+++ commands/gear.js
@@ -0,0 +1,13 @@
+class Cmd {
+
+    constructor(commands, message){
+        this.commands = commands;
+        this.message = message;
+    }
+
+    async run(){
+        
+    }
+}
+
+module.exports = Cmd;
