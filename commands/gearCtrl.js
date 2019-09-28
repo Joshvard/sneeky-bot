0982060cac09bb38bea5a5bde9cf2777053e843d
zjarv <irishstiltskin@gmail.com>
2019-09-28 21:16:41 +0100

    renamed and updated gear functionality, added queries and created a table ind b (migrations will be added later)

---
+++ commands/gearCtrl.js
@@ -0,0 +1,72 @@
+
+class Cmd{
+
+    constructor(command, message, database){
+        this.command = command;
+        this.message = message;
+        this.database = database;
+
+        this.load_register = require("../commands/register.js");
+        this.register = new this.load_register(this.database, this.message, true);
+
+        this.user = {
+            name: "",
+            discord_id: ""
+        };
+
+        let gear_load = require('../lib/gear.js');
+        this.gear = new gear_load(this.database, this.message);
+    }
+
+    // Set the user data for the module
+    set_user_data(user){
+        this.user.name = user.name;
+        this.user.discord_id = user.discord_id;
+    }
+
+    // Set and prepare our gear command, store the extra parameters for processing
+    set_params(params){
+        this.mode = params[0];
+        params.shift();
+        this.params = params;
+    }
+
+    // Similar behaviour for command handling
+    // The first parameter found should always be the 'secondary command' (mode) followed by the parameters required
+    async launch_gear(mode){
+        switch(mode){
+            case 'upload':
+                this.gear.upload(this.user);
+                break;
+
+            case 'download':
+                this.gear.download(this.user);
+                break;
+
+            case 'update':
+                this.gear.update(this.user);
+                break;
+
+            default:
+                this.message.channel.send(`<@${this.user.discord_id}> '${mode}' is not a valid gear command.\n\nPlease use '&help' if you are unsure how to use this command.`);
+                break;
+        }
+    }
+
+    // Run and execute the appropriate gear command if valid
+    // Or else we terminate the command and hopefully explain why
+    async run(){
+        if(!await this.gear.check_player_exists(this.user.discord_id)){
+            let load_register = require("../commands/register.js");
+            let register = new load_register(this.database, this.message, true);
+            register.set_user_data({name: this.user.name, discord_id: this.user.discord_id});
+            register.run();
+
+            this.message.channel.send(`<@${this.user.discord_id}> You don't seem to exist in my database...\nI've registered you ony our behalf, try the command again!`);
+        } else {
+            await this.launch_gear(this.mode);
+        }
+    }
+}
+
+module.exports = Cmd;
